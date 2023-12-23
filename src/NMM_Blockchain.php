d9eeaaccef51c4b7479092002bdc98d70a7e531c
turndealer <turndealer@gmail.com>
2023-12-23 14:51:14 +0530

    Update NMM_Blockchain.php

--- src/NMM_Blockchain.php
+++ src/NMM_Blockchain.php
@@ -1034,6 +1034,55 @@ class NMM_Blockchain {
 
 		return $result;
 	}
+	public static function get_bnb_address_transactions($address) {
+		
+		$request = 'http://api.bscscan.com/api?module=account&action=txlist&address=' . $address . '&startblock=0&endblock=99999999&sort=desc';
+
+		$response = wp_remote_get($request);
+
+		if (is_wp_error($response) || $response['response']['code'] !== 200) {
+			NMM_Util::log(__FILE__, __LINE__, 'FAILED API CALL ( ' . $request . ' ): ' . print_r($response, true));
+
+			$result = array(
+				'result' => 'error',
+				'total_received' => '',
+			);
+
+			return $result;
+		}
+
+		$body = json_decode($response['body']);
+
+		$rawTransactions = $body->result;
+
+		if (!is_array($rawTransactions)) {
+			$result = array(
+				'result' => 'error',
+				'message' => 'No transactions found',
+			);
+
+			return $result;
+		}
+
+		$transactions = array();
+		foreach ($rawTransactions as $rawTransaction) {
+			
+			if (strtolower($rawTransaction->to) === strtolower($address)) {
+				
+				$transactions[] = new NMM_Transaction($rawTransaction->value, 
+													  $rawTransaction->confirmations, 
+													  $rawTransaction->timeStamp,
+													  $rawTransaction->hash);
+			}
+		}
+
+		$result = array (
+			'result' => 'success',
+			'transactions' => $transactions,
+		);
+
+		return $result;
+	}
 
 	public static function get_grs_address_transactions($address) {
 		
